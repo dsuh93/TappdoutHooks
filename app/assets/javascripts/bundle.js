9c367c924733434092c6c0319a6120514ed09ef3
dsuh93 <davidisuh93@gmail.com>
2022-01-22 14:28:37 -0500

    use hooks for splash page

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -70,143 +70,107 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } Object.defineProperty(subClass, "prototype", { value: Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }), writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
+var Splash = function Splash() {
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(""),
+      _useState2 = _slicedToArray(_useState, 2),
+      phoneNumber = _useState2[0],
+      setPhoneNumber = _useState2[1];
 
-var Splash = /*#__PURE__*/function (_React$Component) {
-  _inherits(Splash, _React$Component);
+  var _useState3 = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(false),
+      _useState4 = _slicedToArray(_useState3, 2),
+      showPhoneSubmit = _useState4[0],
+      setPhoneSubmit = _useState4[1];
 
-  var _super = _createSuper(Splash);
+  var handleSubmit = function handleSubmit(e) {
+    e.preventDefault();
+    setPhoneSubmit(true);
+  };
 
-  function Splash(props) {
-    var _this;
+  var handleChange = function handleChange(e) {
+    setPhoneNumber(e.target.value);
+    showPhoneSubmit ? setPhoneSubmit(false) : null;
+  };
 
-    _classCallCheck(this, Splash);
-
-    _this = _super.call(this, props);
-    _this.state = {
-      phoneNumber: "",
-      showPhoneSubmit: false
-    };
-    return _this;
-  }
-
-  _createClass(Splash, [{
-    key: "handleSubmit",
-    value: function handleSubmit(e) {
-      e.preventDefault();
-      this.setState({
-        showPhoneSubmit: true
-      });
+  var phoneSubmitPopUp = showPhoneSubmit ? phoneNumber.length === 10 ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "You've submitted your phone number!"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    onClick: function onClick(e) {
+      return setPhoneSubmit(false);
     }
-  }, {
-    key: "handleChange",
-    value: function handleChange(e) {
-      e.preventDefault();
-      this.setState({
-        phoneNumber: e.target.value
-      });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var _this2 = this;
-
-      var phoneNumber = this.state.phoneNumber;
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-section one"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("nav", {
-        className: "splash-auth"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
-        className: "splash-signin"
-      }, "SIGN IN"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
-        className: "splash-signup"
-      }, "CREATE AN ACCOUNT")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-1"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-1-left"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-        src: "",
-        alt: "logo"
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Discover and share your favorite beer."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("form", {
-        onSubmit: function onSubmit(e) {
-          return _this2.handleSubmit(e);
-        },
-        action: ""
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("input", {
-        onChange: function onChange(e) {
-          return _this2.handleChange(e);
-        },
-        type: "text",
-        value: phoneNumber
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", null, "SEND LINK"))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-1-right"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-        src: window.triplePhones,
-        alt: "triple phones"
-      })))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-section two"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "search bar"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "image-blurbs"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "image-blurb-left"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-        src: "",
-        alt: "left-image"
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, "CHECK IN AND RATE BEER"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Keep track of what you've tried and what you thought of it by checking in a beer and rating it.")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "image-blurb-right"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-        src: "",
-        alt: "right-image"
-      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, "DRINK NEW BEERS, UNLOCK BADGES"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Expand your palate by trying new & different beer styles and unlock achievements along the way.")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-section three"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "splash-three")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-section four"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "splash-four")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-section five"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "splash-five")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "splash-section six"
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "splash-six")));
-    }
-  }]);
-
-  return Splash;
-}(react__WEBPACK_IMPORTED_MODULE_0__.Component);
-
-var mSTP = function mSTP(state, ownProps) {
-  return {};
+  }, "X")) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "Please submit a valid number") : null;
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-section one"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("nav", {
+    className: "splash-auth"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    className: "splash-signin"
+  }, "SIGN IN"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    className: "splash-signup"
+  }, "CREATE AN ACCOUNT")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-1"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-1-left"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: "",
+    alt: "logo"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Discover and share your favorite beer."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("form", {
+    onSubmit: function onSubmit(e) {
+      return handleSubmit(e);
+    },
+    action: ""
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("input", {
+    className: "phone-number-input",
+    onChange: function onChange(e) {
+      return handleChange(e);
+    },
+    placeholder: "(999) 999-9999",
+    type: "text",
+    value: phoneNumber
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", null, "SEND LINK")), phoneSubmitPopUp), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-1-right"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: window.triplePhones,
+    alt: "triple phones"
+  })))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-section two"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "search bar"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "image-blurbs"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "image-blurb-left"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: "",
+    alt: "left-image"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, "CHECK IN AND RATE BEER"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Keep track of what you've tried and what you thought of it by checking in a beer and rating it.")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "image-blurb-right"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    src: "",
+    alt: "right-image"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h4", null, "DRINK NEW BEERS, UNLOCK BADGES"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "Expand your palate by trying new & different beer styles and unlock achievements along the way.")))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-section three"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "splash-three")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-section four"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "splash-four")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-section five"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "splash-five")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "splash-section six"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "splash-six")));
 };
 
-var mDTP = function mDTP(dispatch, ownProps) {
-  return {};
-};
-
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((0,react_redux__WEBPACK_IMPORTED_MODULE_1__.connect)(mSTP, mDTP)(Splash));
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Splash);
 
 /***/ }),
 
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,90 +1,83 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, {useState} from 'react';
 
-class Splash extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      phoneNumber: "",
-      showPhoneSubmit: false
-    };
+const Splash = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [showPhoneSubmit, setPhoneSubmit] = useState(false);
+
+  
+  const handleSubmit =  (e) => {
+    e.preventDefault()
+    setPhoneSubmit(true)
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    this.setState({ showPhoneSubmit: true })
+  const handleChange = (e) => {
+    setPhoneNumber(e.target.value);
+    showPhoneSubmit ? setPhoneSubmit(false) : null
   }
 
-  handleChange(e) {
-    e.preventDefault();
-    this.setState({ phoneNumber: e.target.value })
-  }
+  const phoneSubmitPopUp = showPhoneSubmit ? (
+      phoneNumber.length === 10 ? <div>
+        <div>You've submitted your phone number!</div>
+        <div onClick={(e) => setPhoneSubmit(false)}>X</div>
+      </div> : <div>Please submit a valid number</div>
+  ) : null;
 
-
-  render() {
-    let { phoneNumber } = this.state;
-    return (
-      <div className='splash'>
-        <div className='splash-section one'>
-          <nav className='splash-auth'>
-            <button className='splash-signin'>SIGN IN</button>
-            <button className='splash-signup'>CREATE AN ACCOUNT</button>
-          </nav>
-          <div className='splash-1'>
-            <div className='splash-1-left'>
-              <img src="" alt="logo" />
-              <h1>Discover and share your favorite beer.</h1>
-              <form onSubmit={(e) => this.handleSubmit(e)} action="">
-                <input onChange={(e) => this.handleChange(e)} type="text" value={phoneNumber} />
-                <button>SEND LINK</button>
-              </form>
-            </div>
-            <div className='splash-1-right'>
-              <img src={window.triplePhones} alt="triple phones" />
-            </div>
+  return (
+    <div className='splash'>
+      <div className='splash-section one'>
+        <nav className='splash-auth'>
+          <button className='splash-signin'>SIGN IN</button>
+          <button className='splash-signup'>CREATE AN ACCOUNT</button>
+        </nav>
+        <div className='splash-1'>
+          <div className='splash-1-left'>
+            <img src="" alt="logo" />
+            <h1>Discover and share your favorite beer.</h1>
+            <form onSubmit={(e) => handleSubmit(e)} action="">
+              <input
+                className='phone-number-input'
+                onChange={(e) => handleChange(e)}
+                placeholder='(999) 999-9999'
+                type="text"
+                value={phoneNumber} />
+              <button>SEND LINK</button>
+            </form>
+            {phoneSubmitPopUp}
           </div>
-        </div>
-        <div className='splash-section two'>
-          <div>search bar</div>
-          <div className='image-blurbs'>
-            <div className='image-blurb-left'>
-              <img src="" alt="left-image" />
-              <h4>CHECK IN AND RATE BEER</h4>
-              <p>Keep track of what you've tried and what you thought of it by checking in a beer and rating it.</p>
-            </div>
-            <div className='image-blurb-right'>
-              <img src="" alt="right-image" />
-              <h4>DRINK NEW BEERS, UNLOCK BADGES</h4>
-              <p>Expand your palate by trying new & different beer styles and unlock achievements along the way.</p>
-            </div>
+          <div className='splash-1-right'>
+            <img src={window.triplePhones} alt="triple phones" />
           </div>
-        </div>
-        <div className='splash-section three'>
-          <p>splash-three</p>
-        </div>
-        <div className='splash-section four'>
-          <p>splash-four</p>
-        </div>
-        <div className='splash-section five'>
-          <p>splash-five</p>
-        </div>
-        <div className='splash-section six'>
-          <p>splash-six</p>
         </div>
       </div>
-    )
-  }
+      <div className='splash-section two'>
+        <div>search bar</div>
+        <div className='image-blurbs'>
+          <div className='image-blurb-left'>
+            <img src="" alt="left-image" />
+            <h4>CHECK IN AND RATE BEER</h4>
+            <p>Keep track of what you've tried and what you thought of it by checking in a beer and rating it.</p>
+          </div>
+          <div className='image-blurb-right'>
+            <img src="" alt="right-image" />
+            <h4>DRINK NEW BEERS, UNLOCK BADGES</h4>
+            <p>Expand your palate by trying new & different beer styles and unlock achievements along the way.</p>
+          </div>
+        </div>
+      </div>
+      <div className='splash-section three'>
+        <p>splash-three</p>
+      </div>
+      <div className='splash-section four'>
+        <p>splash-four</p>
+      </div>
+      <div className='splash-section five'>
+        <p>splash-five</p>
+      </div>
+      <div className='splash-section six'>
+        <p>splash-six</p>
+      </div>
+    </div>
+  )
 }
 
-const mSTP = (state, ownProps) => {
-
-  return {}
-}
-const mDTP = (dispatch, ownProps) => {
-
-  return {}
-}
-
-
-
-export default connect(mSTP, mDTP)(Splash)
+export default Splash;
